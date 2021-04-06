6e0bd621246eea65e9ee9c5bc9bb7a266d7ba69b
Nick Gerancher <nick@nicksdesk.com>
2021-04-05 22:31:40 -0400

    format output

--- examples/out.go
+++ examples/out.go
@@ -6,14 +6,8 @@ import (
 
 type MyStruct struct {
 	Name string
-	Age int
+	Age  int
 }
-
-
-
-
-
-
 
 func main() {
 	test()
@@ -21,7 +15,7 @@ func main() {
 
 	my := MyStruct{
 		Name: "Bob",
-		Age: 30,
+		Age:  30,
 	}
 
 	something_gonerics_c1lseh7bm9vl11ce6q60("hello world", &my)
@@ -31,21 +25,21 @@ func test() {
 	example_gonerics_c1lseh7bm9vl11ce6q6g("yo", nil, nil)
 	example_gonerics_c1lseh7bm9vl11ce6q70("hello", nil, nil)
 }
-func cool_gonerics_c1lseh7bm9vl11ce6q5g(num int,age int) (string,int) {
-							return "hello world", age
-					}
-func something_gonerics_c1lseh7bm9vl11ce6q60(arg0 string,arg1 *MyStruct)  {
-							fmt.Printf("%+v\n", arg1.Name)
-					}
-func example_gonerics_c1lseh7bm9vl11ce6q6g(input string,another error,yo *string) (a *string,b string) {
-							fmt.Println(input)
+func cool_gonerics_c1lseh7bm9vl11ce6q5g(num int, age int) (string, int) {
+	return "hello world", age
+}
+func something_gonerics_c1lseh7bm9vl11ce6q60(arg0 string, arg1 *MyStruct) {
+	fmt.Printf("%+v\n", arg1.Name)
+}
+func example_gonerics_c1lseh7bm9vl11ce6q6g(input string, another error, yo *string) (a *string, b string) {
+	fmt.Println(input)
 	fmt.Println(another)
 
 	return yo, input
-					}
-func example_gonerics_c1lseh7bm9vl11ce6q70(input string,another error,yo *string) (a *string,b string) {
-							fmt.Println(input)
+}
+func example_gonerics_c1lseh7bm9vl11ce6q70(input string, another error, yo *string) (a *string, b string) {
+	fmt.Println(input)
 	fmt.Println(another)
 
 	return yo, input
-					}
+}
