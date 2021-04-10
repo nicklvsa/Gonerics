563954b2b6ffa3ea8c767ea633fe41a7917febed
Nick Gerancher <nick@nicksdesk.com>
2021-04-09 22:24:16 -0400

    updates

--- examples/mymax.go
+++ examples/mymax.go
@@ -9,28 +9,28 @@ import (
 
 
 func main() {
-	intVal := myMax_gonerics_c1oduhncsae5lb3krlfg(10, 20)
-	floatVal := myMax_gonerics_c1oduhncsae5lb3krlg0(3.14, 5.64)
+	intVal := myMax_gonerics_c1ogoifcsae3dp7gs3o0(10, 20)
+	floatVal := myMax_gonerics_c1ogoifcsae3dp7gs3og(3.14, 5.64)
 
-	Printer_gonerics_c1oduhncsae5lb3krlgg("Hello World")
+	Printer_gonerics_c1ogoifcsae3dp7gs3p0("Hello World")
 
 	fmt.Printf("%+v\n", intVal)
 	fmt.Printf("%+v\n", floatVal)
 }
-func myMax_gonerics_c1oduhncsae5lb3krlfg(x int,y int) (int) {
+func myMax_gonerics_c1ogoifcsae3dp7gs3o0(x int,y int) (int) {
 							if x > y {
 		return x
 	}
 
 	return y
 					}
-func myMax_gonerics_c1oduhncsae5lb3krlg0(x float64,y float64) (float64) {
+func myMax_gonerics_c1ogoifcsae3dp7gs3og(x float64,y float64) (float64) {
 							if x > y {
 		return x
 	}
 
 	return y
 					}
-func Printer_gonerics_c1oduhncsae5lb3krlgg(data string)  {
+func Printer_gonerics_c1ogoifcsae3dp7gs3p0(data string)  {
 							fmt.Printf("%+v\n", data)
 					}
